f2273f8839be5a0aa4dae067cc9fecaf9a3107fa
nilseuropa <marton@neorobot.hu>
2022-06-20 09:47:25 +0200

    help keys

--- src/main.c
+++ src/main.c
@@ -9,18 +9,36 @@
 #include "mockingboard.h"
 #include "userinterface.h"
 
-unsigned char octave = 3;
+unsigned char octave1 = 0;
+unsigned char octave2 = 0;
 unsigned char key = 0x00;
 unsigned char lastKey = 0x00;
 unsigned int  tonePeriod[] = {1976, 1865, 1760, 1661, 1566, 1480, 1397, 1319, 1245, 1175, 1109, 1047, 0}; // note frequencies rounded up
 
+/*
+
+int pow(int x, unsigned int y)
+{
+    if (y == 0)
+        return 1;
+    else if ((y % 2) == 0)
+        return pow (x, y / 2) * pow (x, y / 2);
+    else
+        return x * pow (x, y / 2) * pow (x, y / 2);
+
+}
+
+// soundDataLeft.tonePeriod[0]=147*pow(2, (tonePeriod[getPeriodIndex(key&0x7F)]/12) );
+
+*/
+
 tMockingSoundRegisters soundDataLeft = {
     { 0, 0, 0 },
     MIN_NOISE_PERIOD,
-    ENABLE_ALL_TONE_CHANNELS,
-    { MAX_AMPLITUDE, MAX_AMPLITUDE, MAX_AMPLITUDE },
-    MIN_ENVELOPE_PERIOD,
-    ENVELOPE_SHAPE_ONE_SHOT_DECAY,
+    ENABLE_CHANNEL(TONE_CHANNEL_A),
+    { MAX_AMPLITUDE, 0, 0 },
+    4096,
+    ENVELOPE_SHAPE_CONT_ATTACK,
     0,
     0
 };
@@ -36,10 +54,16 @@ tMockingSoundRegisters soundDataRight = {
     0
 };
 
+unsigned int  envelopePeriod=0;
+unsigned char envelopeShape=0;
+unsigned char periodIndex=0;
+
 int main(void){
   clrscr();
 
   mockingBoardInit(4);
+  gotoxy(1,21); cprintf("  [s] [d]  [g] [h] [j]");
+  gotoxy(1,22); cprintf("[z] [x] [c] [v] [b] [n] [m]");
 
   while (1){
 
@@ -51,15 +75,30 @@ int main(void){
       gotoxy(0,0);
 
       if (key==lastKey-128) {
-        cprintf("stop     ");
+        cprintf("%d", envelopeShape);
         mockingBoardStopSound(SPEAKER_BOTH);
       }
+      else if (key==219){
+        envelopePeriod-=100;
+      }
+      else if (key==221){
+        envelopePeriod+=100;
+      }
+      else if (key==167){
+        envelopeShape--;
+        if (envelopeShape<1) envelopeShape = 0;
+      }
+      else if (key==220){
+        envelopeShape++;
+        if (envelopeShape>14) envelopeShape=15;
+      }
       else {
-        soundDataLeft.tonePeriod[0]=( (tonePeriod[getPeriodIndex(key&0x7F)]) >> (octave) );
-        soundDataLeft.tonePeriod[1]=( (tonePeriod[getPeriodIndex(key&0x7F)]) >> (octave) );
-        soundDataLeft.tonePeriod[2]=( (tonePeriod[getPeriodIndex(key&0x7F)]) >> (octave) );
+        periodIndex = getPeriodIndex(key&0x7F);
+        soundDataLeft.tonePeriod[0] = ( tonePeriod[periodIndex] >> (octave1) );
+        soundDataLeft.envelopePeriod = envelopePeriod;
+        soundDataLeft.envelopeShape = envelopeShape;
         mockingBoardPlaySound(SPEAKER_BOTH, &soundDataLeft);
-        cprintf("play: %d ", soundDataLeft.tonePeriod[0]);
+        cprintf("%04d Hz", soundDataLeft.tonePeriod[0]);
       }
 
     }
